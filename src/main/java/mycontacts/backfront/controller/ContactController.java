0807c158faaa146ecf0006c65a479cf9bdf6cf22
Dana Poloz <gragik55@gmail.com>
2019-06-16 10:32:20 +0300

    swagger

--- src/main/java/mycontacts/backfront/controller/ContactController.java
+++ src/main/java/mycontacts/backfront/controller/ContactController.java
@@ -1,6 +1,7 @@
 package mycontacts.backfront.controller;
 
 import mycontacts.backfront.model.dto.ContactDto;
+import mycontacts.backfront.model.entity.User;
 import mycontacts.backfront.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,31 @@ public class ContactController {
     public void addContact(@RequestBody ContactDto contactDto){
         contactService.createContact(contactDto);
     }
+
     @DeleteMapping("/deleteContact")
     public void deleteContact(@PathVariable(name = "id") Long id){
         contactService.deleteContact(id);
     }
+
     @GetMapping("/all")
     public List<ContactDto> getAllContacts(){
         return  contactService.getAllContacts();
     }
+
+    @GetMapping("/byPhoneNumber")
+    public ContactDto getContactByPhoneNumber(String phoneNumber){
+        return  contactService.getContactByPhoneNumber(phoneNumber);
+    }
+
+    @GetMapping("/byName")
+    public ContactDto getContactByName(String name){
+        return  contactService.getContactByName(name);
+    }
+
+    @GetMapping("/allNames")
+    public List<User> getAllNames(){
+        return  contactService.getAllNames();
+    }
+
 
 }
